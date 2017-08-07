92327741d08a734a4b7ba7a70e538088ffeb402f
alexandre025 <alexandre.ferraille@gmail.com>
2017-08-07 17:37:50 +0200

    Add option

--- lib/generators/cdx/install/install_generator.rb
+++ lib/generators/cdx/install/install_generator.rb
@@ -5,12 +5,14 @@ module Cdx
 
       desc 'Create a Cdx initializer, copy migrations, mount engine'
 
+      class_option :path, desc: 'Set engine mount path'
+
       def copy_initializer
         template 'cdx.rb', 'config/initializers/cdx.rb'
       end
 
       def mount_engine
-        route "mount Cdx::Engine, at: '/'"
+        route "mount Cdx::Engine, at: '/#{options[:path]}'"
       end
     end
   end

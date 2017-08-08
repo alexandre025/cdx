a5de2406a2ed273ca7555a5bb7b39ce35aef19e8
alexandre025 <alexandre.ferraille@gmail.com>
2017-08-08 15:19:25 +0200

    Main sidebar menu

--- app/helpers/cdx/admin/application_helper.rb
+++ app/helpers/cdx/admin/application_helper.rb
@@ -1,9 +1,20 @@
 module Cdx
   module Admin
     module ApplicationHelper
+      include FontAwesome::Rails::IconHelper
+
+      def context_tag
+        "#{controller_name}_#{action_name}"
+      end
 
       def content_header_page_title
-        t("admin.content_header.page_title.#{controller_name}_#{action_name}")
+        t("admin.content_header.page_title.#{context_tag}")
+      end
+
+      def render_main_sidebar_menu
+        content_for :main_sidebar_menu do
+          render partial: 'cdx/admin/shared/main_sidebar_menu', locals: { current_main_tree: controller_name.to_sym }
+        end
       end
 
     end

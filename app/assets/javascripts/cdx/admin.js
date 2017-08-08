6ecf34a30dd314907041a5ead3c8897dbb3c589f
alexandre025 <alexandre.ferraille@gmail.com>
2017-08-08 15:34:55 +0200

    Disable turbolinks

--- app/assets/javascripts/cdx/admin.js
+++ app/assets/javascripts/cdx/admin.js
@@ -10,8 +10,8 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require turbolinks
 //= require jquery
 //= require bootstrap-sprockets
 //= require cdx/adminlte/adminlte.min
 //= require_tree .
+

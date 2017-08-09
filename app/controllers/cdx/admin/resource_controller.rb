435db06137f1cda7dceef66aac89e3bae317d22b
alexandre025 <alexandre.ferraille@gmail.com>
2017-08-09 12:19:36 +0200

    Init resources controller with Cdx::User for example

--- app/controllers/cdx/admin/resource_controller.rb
+++ app/controllers/cdx/admin/resource_controller.rb
@@ -2,6 +2,12 @@ module Cdx
   module Admin
     class ResourceController < BaseController
 
+      before_action :load_resource
+
+      def index
+
+      end
+
       def new
 
       end
@@ -20,6 +26,59 @@ module Cdx
 
       def destroy
 
+      end
+
+      protected
+
+      class << self
+        attr_accessor :parent_data
+
+        def belongs_to(model_name, options = {})
+          @parent_data ||= {}
+          @parent_data[:model_name] = model_name
+          @parent_data[:model_class] = model_name.to_s.classify.constantize
+          @parent_data[:find_by] = options[:find_by] || :id
+        end
+      end
+
+      def load_resource
+        if member_action? # Load member action
+
+        else # Load collection
+          @collection ||= collection
+          instance_variable_set("@#{controller_name}", @collection)
+        end
+      end
+
+      def collection
+        model_class.where(nil)
+      end
+
+      def resource
+        return @resource if @resource
+        parent_model_name = parent_data[:model_name] if parent_data
+        @resource = Cdx::Admin::Resource.new controller_path, controller_name, parent_model_name, object_name
+      end
+
+      def parent_data
+        self.class.parent_data
+      end
+
+      def model_class
+        @model_class ||= resource.model_class
+      end
+
+      # This method should be overridden when object_name does not match the controller name
+      def object_name
+
+      end
+
+      def collection_actions
+        [:index]
+      end
+
+      def member_action?
+        !collection_actions.include? action_name.to_sym
       end
 
     end
---
+++ app/helpers/cdx/admin/resource_helper.rb
@@ -0,0 +1,11 @@
+module Cdx
+  module Admin
+    module ResourceHelper
+
+      def content_header_page_title
+        t('activerecord.models.cdx/user.other')
+      end
+
+    end
+  end
+end

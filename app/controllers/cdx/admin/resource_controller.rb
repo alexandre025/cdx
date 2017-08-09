d24a3fe73592b6002a8d4438d2d91ae3f10e0d77
alexandre025 <alexandre.ferraille@gmail.com>
2017-08-09 09:42:28 +0200

    Init ressource controller

---
+++ app/controllers/cdx/admin/resource_controller.rb
@@ -0,0 +1,27 @@
+module Cdx
+  module Admin
+    class ResourceController < BaseController
+
+      def new
+
+      end
+
+      def edit
+
+      end
+
+      def update
+
+      end
+
+      def create
+
+      end
+
+      def destroy
+
+      end
+
+    end
+  end
+end

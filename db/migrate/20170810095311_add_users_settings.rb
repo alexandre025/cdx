79a92a49b147850fb6d7775c8bd5cafcbfb6f790
alexandre025 <alexandre.ferraille@gmail.com>
2017-08-10 16:05:45 +0200

    New fields

--- db/migrate/20170810095311_add_users_settings.rb
+++ db/migrate/20170810095311_add_users_settings.rb
@@ -1,5 +1,7 @@
 class AddUsersSettings < ActiveRecord::Migration[5.1]
   def change
+    add_column :cdx_users, :first_name, :string, length: 45
+    add_column :cdx_users, :last_name, :string, length: 45
     add_column :cdx_users, :settings, :hstore
   end
 end

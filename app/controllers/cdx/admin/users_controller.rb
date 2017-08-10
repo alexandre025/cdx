92c856b1c9c39b856e2d0f7eff2d331608014c14
alexandre025 <alexandre.ferraille@gmail.com>
2017-08-10 16:06:12 +0200

    Allow update user without password

--- app/controllers/cdx/admin/users_controller.rb
+++ app/controllers/cdx/admin/users_controller.rb
@@ -2,6 +2,17 @@ module Cdx
   module Admin
     class UsersController < ResourceController
 
+      before_action :update_without_password, only: :update
+
+      private
+
+      def update_without_password
+        if params[:user][:password].blank?
+          params[:user].delete('password')
+          params[:user].delete('password_confirmation')
+        end
+      end
+
     end
   end
 end

1556ad10d53a8d8c25a1cb8e8a10de5ea24700ad
Valentin Soyer <valent.soyer@gmail.com>
2017-08-11 17:15:32 +0200

    Work on taxons form

--- app/controllers/cdx/admin/taxons_controller.rb
+++ app/controllers/cdx/admin/taxons_controller.rb
@@ -1,12 +1,19 @@
 module Cdx
   module Admin
     class TaxonsController < ResourceController
+      # Callbacks
       before_action :load_taxonomy
+      before_action :available_parent_taxons, only: [:new, :edit]
 
+      # Methods
       private
 
       def load_taxonomy
         @taxonomy = Cdx::Taxonomy.find(params[:taxonomy_id])
+      end
+
+      def available_parent_taxons
+        @available_parent_taxons = @object.new_record? ? Cdx::Taxon.by_taxonomy(@taxonomy) : Cdx::Taxon.by_taxonomy(@taxonomy).where.not(id: @object.children.ids.push(@object.id))
       end
     end
   end
--- app/models/cdx/taxon.rb
+++ app/models/cdx/taxon.rb
@@ -4,6 +4,14 @@ module Cdx
     belongs_to :taxonomy
 
     has_many :children, class_name: "Taxon", foreign_key: "parent_id"
-    belongs_to :parent, class_name: "Taxon"
+    belongs_to :parent, class_name: "Taxon", optional: true
+
+    # Scopes
+    scope :by_taxonomy, -> (taxonomy) { where(taxonomy: taxonomy) }
+
+    # Methods
+    def content_header_title
+      name
+    end
   end
 end

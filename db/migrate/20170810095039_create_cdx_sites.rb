03c2791e1f2e574160248862a6b5db3768fdb9ba
alexandre025 <alexandre.ferraille@gmail.com>
2017-08-11 14:50:56 +0200

    Return site’s acronyme to code

--- db/migrate/20170810095039_create_cdx_sites.rb
+++ db/migrate/20170810095039_create_cdx_sites.rb
@@ -2,7 +2,7 @@ class CreateCdxSites < ActiveRecord::Migration[5.1]
   def change
     create_table :cdx_sites do |t|
       t.string :name, limit: 45, null: false
-      t.string :acronym, limit: 45, null: false
+      t.string :code, limit: 45, null: false
       t.string :domain, limit: 45, null: false
       t.json :settings
       t.boolean :default, default: false

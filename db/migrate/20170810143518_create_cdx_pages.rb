a70a1cf03d087c74df97a315ad98e65ea4fa6065
alexandre025 <alexandre.ferraille@gmail.com>
2017-08-11 08:59:45 +0200

    Rework on pages

--- db/migrate/20170810143518_create_cdx_pages.rb
+++ db/migrate/20170810143518_create_cdx_pages.rb
@@ -1,8 +1,11 @@
 class CreateCdxPages < ActiveRecord::Migration[5.1]
   def change
     create_table :cdx_pages do |t|
-      t.string :name, limit: 255, null: false
+      t.string :title, limit: 255, null: false
+      t.string :slug, limit: 255, null: false
       t.text :content
+      t.datetime :published_at
+      t.timestamps
     end
   end
 end

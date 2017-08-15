26312cadbe696c1445465a50e5a5f6d1257714cd
alexandre025 <alexandre.ferraille@gmail.com>
2017-08-15 02:03:06 +0200

    Fix rspec dummy app config path

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 ENV['RAILS_ENV'] ||= 'test'
 
-require File.expand_path('../../config/environment', __FILE__)
+require File.expand_path('../dummy/config/environment.rb', __FILE__)
 
 abort('The Rails environment is running in production mode!') if Rails.env.production?
 

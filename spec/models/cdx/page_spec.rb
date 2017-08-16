b9f0d0b8f4e7dfa4a1346a57d8bb0ba639f90a60
alexandre025 <alexandre.ferraille@gmail.com>
2017-08-16 09:27:31 +0200

    Basic specs for page.rb

---
+++ spec/models/cdx/page_spec.rb
@@ -0,0 +1,32 @@
+require 'rails_helper'
+
+module Cdx
+  RSpec.describe Page, type: :model do
+
+    let(:page) { create(:cdx_page) }
+
+    describe '#save!' do
+      it { expect(build(:cdx_page).save!).to eq(true) }
+      it { expect(page.state).to eq('draft') }
+    end
+
+    describe '#publish' do
+      before :each do
+        page.publish!
+      end
+
+      it { expect(page.state).to eq('published') }
+      it { expect(page.published_on).not_to eq(nil) }
+    end
+
+    describe '#unpublish' do
+      before :each do
+        page.publish!
+        page.unpublish!
+      end
+
+      it { expect(page.state).to eq('draft') }
+    end
+
+  end
+end

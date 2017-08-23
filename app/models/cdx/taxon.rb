01add07e79f7fd970e22b2290686be08e7dd97e9
alexandre025 <alexandre.ferraille@gmail.com>
2017-08-23 16:52:11 +0200

    WIP, some adjustments needed

--- app/models/cdx/taxon.rb
+++ app/models/cdx/taxon.rb
@@ -12,5 +12,21 @@ module Cdx
     def content_header_title
       name
     end
+
+    def full_path_tag
+      parent_path_tag(parent, '')
+    end
+
+    def parent_path_tag(taxon, string)
+      if taxon
+        if string.blank?
+          parent_path_tag(taxon.parent, "#{taxon.name}")
+        else
+          parent_path_tag(taxon.parent, "#{taxon.name} -> #{string}")
+        end
+      else
+        "#{taxonomy.name} -> #{string}"
+      end
+    end
   end
 end

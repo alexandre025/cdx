ddaf237c946ff064c5f72fec60e63de5963c7f2f
alexandre025 <alexandre.ferraille@gmail.com>
2017-08-30 12:31:12 +0200

    Init controller specs

--- spec/controllers/cdx/admin/settings_controller_spec.rb
+++ spec/controllers/cdx/admin/settings_controller_spec.rb
@@ -2,6 +2,27 @@ require 'rails_helper'
 
 module Cdx
   RSpec.describe Admin::SettingsController, type: :controller do
+    routes { Cdx::Engine.routes }
+    render_views
 
+    logged_in
+
+    describe '#GET edit' do
+      it { expect(get :edit).to have_http_status(200) }
+    end
+
+    describe '#PATCH update' do
+      context 'when sucess' do
+        it { expect(patch :update, params: { settings: FactoryGirl.attributes_for(:cdx_setting) }).to have_http_status(302) }
+      end
+
+      context 'when failed' do
+        it { expect(patch :update, params: { settings: '#' }).to have_http_status(302) }
+      end
+    end
+
+    describe '#POST clear_cache' do
+
+    end
   end
 end

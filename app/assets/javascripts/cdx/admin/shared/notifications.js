534cfcb13553740eb916d02b1a7a83f203ac369c
alexandre025 <alexandre.ferraille@gmail.com>
2017-09-03 19:26:32 +0200

    JS

--- app/assets/javascripts/cdx/admin/shared/notifications.js
+++ app/assets/javascripts/cdx/admin/shared/notifications.js
@@ -1,23 +1,30 @@
 Cdx.ready(function () {
 
     $('div[data-toastr]').each(function () {
-        element = $(this);
+        var element = $(this);
+        triggerToastr(element.html(), element.data('toastr'));
 
-        message = element.html();
+    });
 
-        switch (element.data('toastr')) {
-            case 'success':
-                toastr.success(message);
-                break;
-            case 'error':
-                toastr.error(message);
-                break;
-            case 'warning':
-                toastr.warning(message);
-                break;
-            default:
-                toastr.info(message);
-        }
+    $('[data-flash=true]').on('ajax:success', function (event) {
+        var data = event.detail[0];
+        triggerToastr(data['flash']['type'], data['flash']['message']);
     });
 
 });
+
+function triggerToastr(type, message) {
+    switch (type) {
+        case 'success':
+            toastr.success(message);
+            break;
+        case 'error':
+            toastr.error(message);
+            break;
+        case 'warning':
+            toastr.warning(message);
+            break;
+        default:
+            toastr.info(message);
+    }
+}

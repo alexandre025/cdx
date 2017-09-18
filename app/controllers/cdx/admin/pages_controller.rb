f34eca835b2f73cadac190d35f23f1390f39d31f
Valentin Soyer <valent.soyer@gmail.com>
2017-09-18 09:39:48 +0200

    Create a datatable for pages

--- app/controllers/cdx/admin/pages_controller.rb
+++ app/controllers/cdx/admin/pages_controller.rb
@@ -1,6 +1,12 @@
 module Cdx
   module Admin
     class PagesController < PublishableResourceController
+      def index
+        respond_to do |format|
+          format.html
+          format.json { render json: Cdx::PageDatatable.new(view_context) }
+        end
+      end
     end
   end
 end

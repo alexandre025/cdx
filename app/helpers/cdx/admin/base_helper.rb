2d2546783e6080321cda7347eab65349acfb977b
alexandre025 <alexandre.ferraille@gmail.com>
2017-09-25 11:04:29 +0200

    Work arround with Ransack issue where all helpers are available
    Work arround with Ransack issue, where all helpers are available even with our initializer : include_all_helpers = false

--- app/helpers/cdx/admin/base_helper.rb
+++ app/helpers/cdx/admin/base_helper.rb
@@ -19,10 +19,6 @@ module Cdx
         "#{controller_name}_#{action_name}"
       end
 
-      def content_header_page_title
-        t("admin.content_header.page_title.#{context_tag}")
-      end
-
       def render_main_sidebar_menu
         content_for :main_sidebar_menu do
           render partial: 'cdx/admin/shared/main_sidebar_menu', locals: { current_main_tree: controller_name.to_sym }

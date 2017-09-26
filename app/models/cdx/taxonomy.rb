cc9cc04aba4d853b8c33f5dec7dabe706cb81fc1
Valentin Soyer <valent.soyer@gmail.com>
2017-09-26 17:28:27 +0200

    Create available codes for taxonomy

--- app/models/cdx/taxonomy.rb
+++ app/models/cdx/taxonomy.rb
@@ -2,15 +2,18 @@ module Cdx
   class Taxonomy < ApplicationRecord
     include Cdx::Admin::Resourceable
 
+    # Callbacks
+    after_create :set_root
+
     # Associations
     has_many :taxons, dependent: :destroy
-
     has_one :root, -> { where parent_id: nil }, class_name: 'Cdx::Taxon', dependent: :destroy
-
-    after_create :set_root
 
     # Validators
     validates :name, presence: true
+
+    # Constants
+    AVAILABLE_CODES = %w(pages)
 
     # Methods
     def content_header_title

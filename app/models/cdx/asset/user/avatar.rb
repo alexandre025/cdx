e2a3f8a8e032928cc593d9ec8b26b4087dc5b9d8
Valentin Soyer <valent.soyer@gmail.com>
2017-09-28 09:58:21 +0200

    User avatar style in form

--- app/models/cdx/asset/user/avatar.rb
+++ app/models/cdx/asset/user/avatar.rb
@@ -2,8 +2,6 @@ module Cdx
   class Asset::User::Avatar < Asset
     has_attached_file :attachment,
                       styles:  { thumb: '100x100^', regular: '250x250^' },
-                      default_style:  :regular,
-                      default_url:    '/default/user/avatar/:style.png',
                       url:            '/cdx/user/avatar/:id/:style/:basename.:extension',
                       path:           ':rails_root/public/cdx/user/avatar/:id/:style/:basename.:extension',
                       convert_option: { all: '-strip -crop -gravity center -auto-orient' }

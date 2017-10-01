b3e4500cb99c367d4b6089b2ca35d80118a8ef28
alexandre025 <alexandre.ferraille@gmail.com>
2017-10-01 14:13:31 +0200

    Fix datatables error in vendors

--- app/assets/javascripts/cdx/admin/controllers/users.js
+++ app/assets/javascripts/cdx/admin/controllers/users.js
@@ -1,14 +1,21 @@
 Cdx.ready(function () {
-  $('#users-table').dataTable({
-    "processing": false,
-    "serverSide": true,
-    "ajax": $('#users-table').data('source'),
-    "pagingType": "full_numbers",
-    "columns": [
-      {"data": "email"},
-      {"data": "first_name"},
-      {"data": "last_name"},
-      {"data": "actions", "orderable": false}
-    ]
-  });
+
+    if ($('#users_index').is('*')) {
+
+        $table = $('#users-table');
+
+        $table.dataTable({
+            "processing": false,
+            "serverSide": true,
+            "ajax": $table.data('source'),
+            "pagingType": "full_numbers",
+            "columns": [
+                {"data": "email"},
+                {"data": "first_name"},
+                {"data": "last_name"},
+                {"data": "actions", "orderable": false}
+            ]
+        });
+
+    }
 });

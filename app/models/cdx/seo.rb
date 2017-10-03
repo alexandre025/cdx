f8fce7ffd2272bbb31800432e6ae3421a97874bf
alexandre025 <alexandre.ferraille@gmail.com>
2017-10-03 11:45:45 +0200

    Nullify seo blank values

--- app/models/cdx/seo.rb
+++ app/models/cdx/seo.rb
@@ -9,5 +9,16 @@ module Cdx
     store_accessor :meta, :meta_title, :meta_description, :meta_keywords, :meta_canonical
     store_accessor :og, :og_title, :og_description, :og_keywords, :og_video
 
+    after_validation :normalize_blank_values
+
+    def normalize_blank_values
+      attributes['meta'].each do |column, value|
+        self['meta'][column].present? || self['meta'][column] = nil
+      end
+      attributes['og'].each do |column, value|
+        self['og'][column].present? || self['og'][column] = nil
+      end
+    end
+
   end
 end

63521ba6b6f9dede6df5107d3216643d6c0dd985
alexandre025 <alexandre.ferraille@gmail.com>
2017-10-06 09:05:06 +0200

    Add template column to pages

--- spec/dummy/db/schema.rb
+++ spec/dummy/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171002130330) do
+ActiveRecord::Schema.define(version: 20171005095457) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,6 +36,7 @@ ActiveRecord::Schema.define(version: 20171002130330) do
     t.date "published_on"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "template", default: "default"
   end
 
   create_table "cdx_seos", force: :cascade do |t|

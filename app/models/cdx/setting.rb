618daea9ca1fc2bcaf5b322d95e4eca26b016405
alexandre025 <alexandre.ferraille@gmail.com>
2017-10-06 15:16:55 +0200

    Theme selector

--- app/models/cdx/setting.rb
+++ app/models/cdx/setting.rb
@@ -2,6 +2,10 @@ module Cdx
   class Setting < ApplicationRecord
     include Cdx::SeoSupport
 
+    # Constants
+
+    AVAILABLE_THEMES = %w(black-light black blue-light blue green-light green purple-light purple red-light red yellow-light yellow).freeze
+
     # Validators
 
     validates :acronym, :default_theme, :default_locale, presence: true
@@ -17,7 +21,7 @@ module Cdx
     end
 
     def self.current
-      Setting.first_or_create(acronym: 'CDX', default_theme: 'yellow', default_locale: 'en', available_locales: %w(en fr es))
+      Setting.first_or_create(acronym: 'CDX', default_theme: AVAILABLE_THEMES.last, default_locale: 'en', available_locales: %w(en fr es))
     end
   end
 end
